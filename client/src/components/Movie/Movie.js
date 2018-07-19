5ac58db66e4dd74c5bc1d4804ab60a9205ff2caa
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-07-19 17:01:43 +0300

    start

--- client/src/components/Movie/Movie.js
+++ client/src/components/Movie/Movie.js
@@ -13,13 +13,15 @@ const Movie = (props) => {
             <div className={"movie-container " + movie.tomatoIcon}>
                 <div className="poster" style={{backgroundImage: `url(${movie.posters.primary})`}}/>
                 <div className={"body"}>
-                    <div className="title">{movie.title}</div>
-                    <div className="tomato">
-                        <img src={icons[movie.tomatoIcon]}/>
-                        {movie.tomatoScore}%
+                    <div className="title p-1">{movie.title}</div>
+                    <div className={"p-1"}>
+                        <div className="tomato">
+                            <img src={icons[movie.tomatoIcon]}/>
+                            {movie.tomatoScore}%
+                        </div>
+                        <p>{movie.theaterReleaseDate}</p>
+                        <p>Actors: {movie.actors.join(', ')}</p>
                     </div>
-                    <p>{movie.theaterReleaseDate}</p>
-                    <p>Actors: {movie.actors.join(', ')}</p>
                 </div>
             </div>
         </div>
