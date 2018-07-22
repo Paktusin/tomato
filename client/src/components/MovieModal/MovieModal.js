015466ea416252008f88323c71611641534ee20d
Mikahil <paktusin@gmail.com>
2018-07-22 11:04:55 +0300

    find

--- client/src/components/MovieModal/MovieModal.js
+++ client/src/components/MovieModal/MovieModal.js
@@ -16,14 +16,14 @@ class MovieModal extends React.Component {
 
     render() {
         const movie = this.props.movie;
-        let style = {height:70};
-        if (this.state.image) style = {backgroundImage: `url(${this.state.image})`,height:290}
+        let style = {height: 70};
+        if (this.state.image) style = {backgroundImage: `url(${this.state.image})`, height: 290}
         return (
             <div className="movie-modal">
                 <div className={"poster"} style={style}>
                     <h2 className={"title"}>{movie.title}</h2>
                 </div>
-                <div className={"p-2"}>
+                <div className={"p-3"}>
                     <p><span className="text-primary">Theater:</span> {movie.theaterReleaseDate}</p>
                     <p><span className="text-primary">DVD:</span> {movie.dvdReleaseDate}</p>
                     <p><span className="text-primary">Rating:</span> {movie.mpaaRating}</p>
@@ -33,14 +33,14 @@ class MovieModal extends React.Component {
                         <span className="text-primary">Review:</span> <span
                         dangerouslySetInnerHTML={{__html: movie.synopsis}}/>
                     </p>
-                    {movie.mainTrailer &&
-                    <div className={"trailer"}>
-                        <video controls="controls">
-                            <source src={movie.mainTrailer.sourceId}/>
-                            Your browser doesn't support HTML5 video tag.
-                        </video>
-                    </div>}
                 </div>
+                {movie.mainTrailer &&
+                <div className={"trailer"}>
+                    <video controls="controls">
+                        <source src={movie.mainTrailer.sourceId}/>
+                        Your browser doesn't support HTML5 video tag.
+                    </video>
+                </div>}
             </div>
         )
     }
