1c110f822a6dcb64ed5ce301fa73f344a6dadbae
Mikahil <paktusin@gmail.com>
2018-07-29 15:08:04 +0300

    favicon and ag

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,8 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
+    },
     entry: {
         app: './src/index.js'
     },
@@ -44,6 +57,9 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "[name].[hash].css",
         }),
+        new CopyWebpackPlugin([{
+            from:'public',to:path.resolve(__dirname, 'dist')
+        }]),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
